2c57795a142af1bdef3fc8775c3fb496c80ab062
Adam Awad <awada@wit.edu>
2015-01-20 10:56:39 -0500

    Fizzbuzz!

---
+++ fizzbuzz.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int x = 1; x++; x <= 100)
+	{
+		if (x % 3 = 0 && x % 5 = 0)
+		{
+			cout << "FizzBuzz" << endl;
+		}
+		else if (x % 3 = 0)
+		{
+			cout << "Fizz" << endl;
+		}
+		else if (x % 5 = 0)
+		{
+			cout << "Buzz" << endl;
+		}
+	}
+}
